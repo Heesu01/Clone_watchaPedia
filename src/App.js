7adaa2a759adaa419b0b6f84f26a757da4ad9b46
Heesu01 <rulru0413@naver.com>
2024-09-08 19:16:54 +0900

    feat: 라우트 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Navigate to="/Home" />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Movie/:id" element={<Movie />} />
+      </Routes>
     </div>
   );
 }
