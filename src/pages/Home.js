6bc2447b398caf03b1ad9517f495aaec748c9820
Heesu01 <rulru0413@naver.com>
2024-09-10 20:58:10 +0900

    feat: 메인페이지 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import Banner from "../components/Banner";
+import Rank from "../components/Rank";
+import Upcoming from "../components/Upcoming";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Banner />
+      <Rank />
+      <Upcoming />
+    </Container>
+  );
 };
 
+const Container = styled.div`
+  width: 93%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`;
 export default Home;
